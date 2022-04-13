f4935884281391b5355425c53e2d2bc0624d7942
MAVOTUNA <sdm03131229@gmail.com>
2022-04-13 13:44:25 +0900

    ㄱㄱ

--- src/main/java/helloec2/demo/Controller.java
+++ src/main/java/helloec2/demo/Controller.java
@@ -1,5 +1,6 @@
 package helloec2.demo;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -7,10 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Controller {
 
     @GetMapping("test")
-    @ResponseBody
     public String test() {
-        return "Hello ec2";
+        return "index";
     }
-
 
 }
